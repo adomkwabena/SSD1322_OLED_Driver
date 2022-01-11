ac3e8e782dfb7ca06ada817c142cbe61831b2e19
adomkwabena <26465269+adomkwabena@users.noreply.github.com>
2022-01-11 18:16:36 +0000

    Clean up main file

--- src/main.c
+++ src/main.c
@@ -7,7 +7,9 @@
 // ****************************************************************************
 // * Include Files.
 // ****************************************************************************
+
 #include "stm32f407xx.h"
+#include "gpio.h"
 
 // ****************************************************************************
 // * Application Implementation.
@@ -15,23 +17,14 @@
 
 int main(void)
 {
-    // Enable GPIOD clock
-    RCC->AHB1ENR   |= RCC_AHB1ENR_GPIODEN;
-    // Configure GPIOD pins 12-15 as output
-    GPIOD->MODER   |= GPIO_MODER_MODER15_0 | GPIO_MODER_MODER14_0 |\
-                      GPIO_MODER_MODER13_0 | GPIO_MODER_MODER12_0;
-    // Configure all GPIOD pins as push pull
-    GPIOD->OTYPER   = 0x00000000;
-    // Configure all GPIOD pins as no pull
-    GPIOD->PUPDR    = 0x00000000;
+    gpio_init();
 
-
-    // Turn on LEDs on PD15, PD14, PD13 and PD12.
     while (1)
     {
-        GPIOD->BSRR |= GPIO_BSRR_BS15;
-        GPIOD->BSRR |= GPIO_BSRR_BS14;
-        GPIOD->BSRR |= GPIO_BSRR_BS13;
-        GPIOD->BSRR |= GPIO_BSRR_BS12;
+        // Turn all LEDs on
+        RED_LED_ON();
+        BLUE_LED_ON();
+        GREEN_LED_ON();
+        ORANGE_LED_ON();
     }
 }

d1e6cdf4cc46770fcc2e5ef36616e3e5613bedc5
adomkwabena <26465269+adomkwabena@users.noreply.github.com>
2022-02-14 14:19:54 +0000

    Add i2c1 demo code

---
+++ include/core_drivers/i2c1/i2c1_test.h
@@ -0,0 +1,21 @@
+/** 
+ *  @file   i2c1_test.h
+ *  @author Adom Kwabena
+ *  @brief  This file implements test routines for the I2C1 module.
+ *          All test data can be verified via an oscilloscope or logic analyzer.
+ */
+
+// Prevent multiple file inclusion.
+#ifndef     __I2C1_TEST_INC__
+#define     __I2C1_TEST_INC__
+
+// ****************************************************************************
+// * Function Prototypes
+// ****************************************************************************
+
+/**
+ *  @brief Generate an i2c1 demo using connected peripherals.
+ */
+void i2c1_demo(void);
+
+#endif
